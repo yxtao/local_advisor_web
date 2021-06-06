5419af98e074a72a9a063889f114d898c2108e5b
yxtao <yinxiatao@gmail.com>
2021-06-05 19:13:35 -0700

    Add Toolbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React  from 'react';
 import { Container } from '@material-ui/core';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import NavBar from './components/NavBar/navBar'
-import Home from './components/Home/Home'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import NavBar from './components/NavBar/navBar';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 const App = () => {      
     return (
       <BrowserRouter>
@@ -10,6 +11,7 @@ const App = () => {
             <NavBar />
             <Switch>
                <Route path="/" exact component={Home} />
+               <Route path="/auth" exact component={Auth} />
             </Switch>
             <Home /> 
          </Container>
--- client/src/components/NavBar/navBar.js
+++ client/src/components/NavBar/navBar.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import useStyles from './styles';
 import ideasPicture from '../../images/ideas.png'
-import { AppBar, Typography } from '@material-ui/core'
+import { AppBar, Typography, Toolbar, Button } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 
 const NavBar = ()=>{
     const classes = useStyles();
+    const user = null;
     return(
         <AppBar className={classes.appbar} position = "static" color="inherit">
             <div >
                <Typography component ={Link} to="/" className={classes.heading} variant="h3" align="center">Local Advisor  
                <img className={ideasPicture.image} src = {ideasPicture} alt="ideas" height="30" />
                </Typography>
-            </div>   
+            </div>  
+            <Toolbar>
+                {user ? (
+                    <div>TODO: Show user info</div>
+                ) : (<Button component= {Link} to="/auth" color="primary">Sign up</Button>)
+                }
+            </Toolbar> 
          </AppBar>
     )
 }
